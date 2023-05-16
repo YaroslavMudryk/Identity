f0583cf6a3468ba9b5b68de0be2994d8ea977c5e
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2023-05-16 22:03:40 +0300

    All handlers

--- Identity/Extensions/IdentityOptionsExtensions.cs
+++ Identity/Extensions/IdentityOptionsExtensions.cs
@@ -11,18 +11,18 @@ namespace Identity.Extensions
 
             handlers.Add(new DisplayRoutesHandler(identityOptions));
 
-            //handlers.Add(new RegisterHandler(identityOptions));
-            //handlers.Add(new LoginHandler(identityOptions));
-            //handlers.Add(new Login2MfaHandler(identityOptions));
-            //handlers.Add(new RefreshTokenHandler(identityOptions));
-            //handlers.Add(new LogoutHandler(identityOptions));
-            //handlers.Add(new SendConfirmHandler(identityOptions));
-            //handlers.Add(new ConfirmHandler(identityOptions));
-            //handlers.Add(new Enable2MfaHandler(identityOptions));
-            //handlers.Add(new Disable2MfaHandler(identityOptions));
-            //handlers.Add(new ChangeLoginHandler(identityOptions));
-            //handlers.Add(new ChangePasswordHandler(identityOptions));
-            //handlers.Add(new RetrieveSessionsHandler(identityOptions));
+            handlers.Add(new RegisterHandler(identityOptions));
+            handlers.Add(new LoginHandler(identityOptions));
+            handlers.Add(new Login2MfaHandler(identityOptions));
+            handlers.Add(new RefreshTokenHandler(identityOptions));
+            handlers.Add(new LogoutHandler(identityOptions));
+            handlers.Add(new SendConfirmHandler(identityOptions));
+            handlers.Add(new ConfirmHandler(identityOptions));
+            handlers.Add(new Enable2MfaHandler(identityOptions));
+            handlers.Add(new Disable2MfaHandler(identityOptions));
+            handlers.Add(new ChangeLoginHandler(identityOptions));
+            handlers.Add(new ChangePasswordHandler(identityOptions));
+            handlers.Add(new RetrieveSessionsHandler(identityOptions));
             handlers.Add(new CloseSessionHandler(identityOptions));
 
             return handlers;
